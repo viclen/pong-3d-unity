5a053bef2ac1e824e9d7fafda76cba84777f0775
Victor <victorpbi5@hotmail.com>
2020-04-25 18:33:43 -0400

    fisica

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public float velocidade = 10;
+    public GameObject MainCamera;
+    float velocidadeatual;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 dir = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y).normalized;
+        GetComponent<Rigidbody2D>().velocity = dir * velocidadeatual;
+
+        velocidadeatual += 0.1f;
+
+        if (col.gameObject.name == "ParedeDireita")
+        {
+            // gol player 1
+            MainCamera.GetComponent<Main>().SomarPonto(1);
+            MainCamera.GetComponent<Main>().Reiniciar();
+        }
+        else if (col.gameObject.name == "ParedeEsquerda")
+        {
+            // gol player 2
+            MainCamera.GetComponent<Main>().SomarPonto(2);
+            MainCamera.GetComponent<Main>().Reiniciar();
+        }
+
+    }
+
+    public void jogar()
+    {
+        velocidadeatual = velocidade;
+        float x = Random.Range(0, 10) > 5 ? 1 : -1;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x, Random.Range(-1, 1)) * velocidadeatual;
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public Text pontosPlayer1;
+    public Text pontosPlayer2;
+    public Text contador;
+
+    public GameObject bola;
+    public GameObject player1;
+    public GameObject player2;
+
+    void Start()
+    {
+        StartCoroutine(JogarCoroutine());
+    }
+
+    public void Reiniciar()
+    {
+        bola.transform.position = new Vector3(0, 0, 0);
+        bola.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        player1.transform.position = new Vector3(player1.transform.position.x, 0, 0);
+        player2.transform.position = new Vector3(player2.transform.position.x, 0, 0);
+
+        StartCoroutine(JogarCoroutine());
+    }
+
+    public void SomarPonto(int player)
+    {
+        if (player == 1)
+        {
+            pontosPlayer1.text = (int.Parse(pontosPlayer1.text) + 1) + "";
+        }
+        else
+        {
+            pontosPlayer2.text = (int.Parse(pontosPlayer2.text) + 1) + "";
+        }
+    }
+
+    IEnumerator JogarCoroutine()
+    {
+        contador.text = "3";
+        yield return new WaitForSeconds(1);
+        contador.text = "2";
+        yield return new WaitForSeconds(1);
+        contador.text = "1";
+        yield return new WaitForSeconds(1);
+        contador.text = "";
+        bola.GetComponent<Bola>().jogar();
+    }
+}
